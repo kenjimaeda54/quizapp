4fa52100b072b34d7eff95634a90286e0ef12a02
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-11-08 18:31:39 -0300

    preparing enviroment

---
+++ src/theme/index.tsx
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    primary: '#1B1BF',
+    background_primary: '#F4F5F6',
+    white: '#FFFF',
+    text: '#7A7A80',
+    main: '#DC1637',
+    main_light: '#FDEDEF',
+    success: '#03b252',
+    shape: '#E1E1E8',
+    shape_dark: '#29292E',
+    black: '#1B1B1F',
+  },
+
+  fonts: {
+    archivo_500: 'Archivo_500Medium',
+    archivo_400: 'Archivo_400Regular',
+    archivo_600: 'Archivo_600SemiBold',
+  },
+};
